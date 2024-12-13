2ac948c125337d9cdbe37f38064891c2dcbf22c0
Afonso Monteiro <affonsoh.monteiro@gmail.com>
2024-12-13 20:36:13 -0300

    adicionando React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
 
 
 
@@ -9,7 +11,9 @@ export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}> 
-      
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/pages/History.tsx
@@ -0,0 +1,6 @@
+
+export function History() {
+  return (
+    <h1> Histórico </h1>
+  )
+}
