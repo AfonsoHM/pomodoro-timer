ce2706ec43166d3137cd1c055d249c77beb794ed
Afonso Monteiro <affonsoh.monteiro@gmail.com>
2025-01-25 22:19:28 -0300

    update

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useReducer, useState } from "react";
-import { ActionTypes, Cycle, cycleReducer } from "../reducers/cycles";
+import { Cycle, cycleReducer } from "../reducers/cycles/reducer";
+import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
 
 interface CreateCycleData {
   task: string;
@@ -46,12 +47,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     }
   
     function markCurrentCycleAsFinished() {
-      dispatch({
-        type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-        payload: {
-          activeCycleId,
-        }
-      })
+      dispatch(markCurrentCycleAsFinishedAction())
         // setCycles((state) => state.map((cycle) => {
         //     if(cycle.id === activeCycleId) {
         //       return {...cycle, finishDate: new Date()}
@@ -72,12 +68,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
         startDate: new Date(),
       }
   
-      dispatch({
-        type: ActionTypes.ADD_NEW_CYCLE,
-        payload: {
-          newCycle,
-        }
-      })
+      dispatch(addNewCycleAction(newCycle))
       // setCycles((state) => [...state, newCycle])
       setAmountSecondsPassed(0)
   
@@ -93,12 +84,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
       //   }),
       // )
 
-      dispatch({
-        type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-        payload: {
-          activeCycleId,
-        }
-      })
+      dispatch(interruptCurrentCycleAction())
 
     }
 
--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -9,7 +9,7 @@ export function History() {
 
   return (
     <HistoryContainer> 
-      <h1>Meu histórico</h1>bbb
+      <h1>Meu histórico</h1>
 
       <HistoryList>
         <table>
